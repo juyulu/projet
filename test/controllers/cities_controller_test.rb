0bce05deb858694d40d109e5ba0d5f8bce2c82d0
juyulu <celine916yulu@gmail.com>
2016-10-28 15:28:08 +0200

    Update cities_controller_test.rb

--- test/controllers/cities_controller_test.rb
+++ test/controllers/cities_controller_test.rb
@@ -1,52 +1,56 @@
 require 'test_helper'
 
 class CitiesControllerTest < ActionController::TestCase
-  setup do
+    setup do
     @city = cities(:one)
-  end
+end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:cities)
-  end
+test "should get index" do
+    VCR.use_cassette("forecast") do
+        get :index
+        assert_response :success
+        assert_not_nil assigns(:cities)
+    end
+end
 
-  test "should get new" do
+test "should get new" do
     get :new
     assert_response :success
-  end
-  
-  test "should create city" do
+end
+
+test "should create city" do
     VCR.use_cassette("nominatim") do
-      assert_difference('City.count') do
-        post :create, city: { lat: @city.lat, lon: @city.lon, name: @city.name }
-      end
-      assert_redirected_to city_path(assigns(:city))
+        assert_difference('City.count') do
+            post :create, city: { lat: @city.lat, lon: @city.lon, name: @city.name }
+        end
+        assert_redirected_to city_path(assigns(:city))
     end
-  end
+end
 
-  test "should show city" do
-    get :show, id: @city
-    assert_response :success
-  end
+test "should show city" do
+    VCR.use_cassette("forecast") do
+        get :show, id: @city
+        assert_response :success
+    end
+end
 
-  test "should get edit" do
+test "should get edit" do
     get :edit, id: @city
     assert_response :success
-  end
+end
 
-  test "should update city" do
+test "should update city" do
     VCR.use_cassette("nominatim") do
-      patch :update, id: @city, city: { lat: @city.lat, lon: @city.lon, name: @city.name }
-      assert_redirected_to city_path(assigns(:city))
+        patch :update, id: @city, city: { lat: @city.lat, lon: @city.lon, name: @city.name }
+        assert_redirected_to city_path(assigns(:city))
     end
-  end
+end
 
-  test "should destroy city" do
+test "should destroy city" do
     assert_difference('City.count', -1) do
-      delete :destroy, id: @city
+        delete :destroy, id: @city
     end
-
+    
     assert_redirected_to cities_path
-  end
+end
 end
