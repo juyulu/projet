5c615ef24be86f674189dc8b2069e7ef1df9f2d8
celine916yulu <celine916yulu@gmail.com>
2016-11-26 06:44:40 -0500

    ...

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ![travis](https://travis-ci.org/juyulu/projet.svg?branch=master)
+![Code Climate](https://codeclimate.com/github/juyulu/projet/badges/gpa.svg)
+![Test Coverage](https://codeclimate.com/github/juyulu/projet/badges/coverage.svg)
 # projet
 Projet de l'annee
 
--- test/models/city_test.rb
+++ test/models/city_test.rb
@@ -4,14 +4,5 @@ class CityTest < ActiveSupport::TestCase
   setup do
     @city = cities(:one)
     @cityWrong = cities(:two)
-  end
-
-  test 'weather forecast' do 
-     VCR.use_cassette("weather") do
-       weather = cities(:one).weather
-       puts weather
-       assert cities(:one).weather
-     end
-  end
-  
+  end  
 end
